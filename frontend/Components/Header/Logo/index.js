9d2ff23d2a603dfe529fac62f7bb50efff89cfd8
opilewski <owen16@vt.edu>
2019-05-15 22:15:55 -0400

    Add files via upload

---
+++ frontend/Components/Header/Logo/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './styles';
+
+const Logo = () => {
+  return (
+    <div className={styles.logoContainer}>
+      <div className={styles.logoTitle}>Welcome to Betterdep!</div>
+    </div>
+  );
+};
+
+export default Logo;

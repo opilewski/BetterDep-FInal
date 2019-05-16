5d30652655dbc656c9c1ecf63732a298c538a06a
opilewski <owen16@vt.edu>
2019-05-15 22:13:17 -0400

    Add files via upload

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: '8d0592a7fd7a0b17f861',
+  GITHUB_CLIENT_SECRET: '86c88b6270b99782b3f2d0d8e16c0a7d8f71dcaf',
+  GITHUB_CALLBACK_URL: 'http://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/reforum',
+};
